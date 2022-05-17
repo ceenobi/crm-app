b986519205e98499e7edf2982be55fcf3e6f930d
ceenobi <cobimbachu@gmail.com>
2022-05-17 23:02:37 +0100

    fixes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -33,45 +33,6 @@ export default function Dashboard() {
     setCategories([...new Set(tickets?.map(({category})=>category))])
   }, [tickets, setCategories])
 
-  // const tickets = [
-  //   {
-  //     category: 'Q1 2020',
-  //     title: 'NFT Safety tips',
-  //     owner: 'Cobi Mac',
-  //     avatar:
-  //       'https://res.cloudinary.com/ceenobi/image/upload/v1629976310/Gadgets/bbd.hpthumb.fazepowerbeatspro.desktoptall.jpg.large.2x_c8lo3p.jpg',
-  //       status: 'done',
-  //       priority: 5,
-  //       progress: 40,
-  //       description: 'show how to make nft',
-  //       timeStamp: '2022-05-13T07:36:17+0000'
-  //   },
-  //   {
-  //     category: 'Q1 2020',
-  //     title: 'Crypto Safety tips',
-  //     owner: 'Ceenobi',
-  //     avatar:
-  //       'https://res.cloudinary.com/ceenobi/image/upload/v1629976310/Gadgets/bbd.hpthumb.fazepowerbeatspro.desktoptall.jpg.large.2x_c8lo3p.jpg',
-  //       status: 'stuck',
-  //       priority: 3,
-  //       progress: 70,
-  //       description: 'show how to trade crypto even if you are a novice',
-  //       timeStamp: '2022-05-12T07:36:17+0000'
-  //   },
-  //   {
-  //     category: 'Q2 2020',
-  //     title: 'Sda Safety tips',
-  //     owner: 'Ceenobi',
-  //     avatar:
-  //       'https://res.cloudinary.com/ceenobi/image/upload/v1629976310/Gadgets/bbd.hpthumb.fazepowerbeatspro.desktoptall.jpg.large.2x_c8lo3p.jpg',
-  //       status: 'working on it',
-  //       priority: 5,
-  //       progress: 60,
-  //       description: 'show how to trade crypto even if you are a novice',
-  //       timeStamp: '2022-05-15T07:36:17+0000'
-  //   },
-  // ]
-
   const colors = ['#BB6464', '#346751', '#C84B31', '#ECDBBA']
 
   const uniqueCategories = [
